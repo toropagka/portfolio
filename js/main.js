3fc407e2c90824177fc57102ce8fb17ae2474d5d
DmitriyZ <toropagka@gmail.com>
2022-07-25 15:08:54 +0300

    first services card

--- js/main.js
+++ js/main.js
@@ -3,9 +3,12 @@
 // import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
 
 const popUpRemove = document.querySelector(".pop-up__remove");
+const servicesPopUpRemove = document.querySelector(".services-pop-up__remove");
 const hiddenCard = document.querySelector(".hidden-card");
 const btnContact = document.querySelectorAll(".button-contact");
+const btnMore = document.querySelectorAll(".button-more");
 const container = document.querySelector(".overlay-container");
+const servicesContainer = document.querySelector(".services-container");
 const benefitsBlock = document.querySelectorAll(".benefits__block");
 const benefitsBlockNumber = document.querySelectorAll(
   ".benefits__block_number"
@@ -13,15 +16,23 @@ const benefitsBlockNumber = document.querySelectorAll(
 const video = document.querySelectorAll(".slider-video");
 const body = document.getElementsByTagName("body");
 
-popUpRemove.addEventListener("click", () => {
-  container.classList.remove("active");
-});
-
 for (const item of btnContact) {
   item.addEventListener("click", () => {
     container.classList.add("active");
   });
 }
+popUpRemove.addEventListener("click", () => {
+  container.classList.remove("active");
+});
+
+for (const item of btnMore) {
+  item.addEventListener("click", () => {
+    servicesContainer.classList.add("active");
+  });
+}
+servicesPopUpRemove.addEventListener("click", () => {
+  servicesContainer.classList.remove("active");
+});
 
 benefitsBlock.forEach((element, index) => {
   element.onmouseover = function () {
